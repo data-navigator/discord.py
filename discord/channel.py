58b7c8b03e27f54651ae09b94a86b4e765a64f8d
Clinton Dow <clint@datanavigator.ca>
2019-03-19 17:07:01 -0700

    Revert "added .idea to gitignore"
    This reverts commit ac7d4331cffc677159f3393d43865efd43741ed4.

--- discord/channel.py
+++ discord/channel.py
@@ -83,7 +83,7 @@ class Channel(Hashable):
 
     __slots__ = [ 'voice_members', 'name', 'id', 'server', 'topic', 'position',
                   'is_private', 'type', 'bitrate', 'user_limit',
-                  '_permission_overwrites', 'parent_id', 'nsfw' ]
+                  '_permission_overwrites' ]
 
     def __init__(self, **kwargs):
         self._update(**kwargs)
@@ -91,9 +91,6 @@ class Channel(Hashable):
 
     def __str__(self):
         return self.name
-
-    def __await__(self):
-        pass
 
     def _update(self, **kwargs):
         self.name = kwargs.get('name')
@@ -105,8 +102,6 @@ class Channel(Hashable):
         self.bitrate = kwargs.get('bitrate')
         self.type = kwargs.get('type')
         self.user_limit = kwargs.get('user_limit')
-        self.parent_id = kwargs.get('parent_id', None)
-        self.nsfw = kwargs.get('nsfw', False)
         try:
             self.type = ChannelType(self.type)
         except:
