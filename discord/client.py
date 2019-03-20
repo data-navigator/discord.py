b7f17e6ad53c7a3f9ac57d8c773430b3022f7a32
Clinton Dow <clint@datanavigator.ca>
2019-03-19 17:10:37 -0700

    Added parent parameter to Client.create_channel to set channel category

--- discord/client.py
+++ discord/client.py
@@ -2062,7 +2062,7 @@ class Client:
         yield from self.http.move_channel_position(channel.server.id, payload)
 
     @asyncio.coroutine
-    def create_channel(self, server, name, *overwrites, type=None):
+    def create_channel(self, server, name, *overwrites, type=None, parent=None):
         """|coro|
 
         Creates a :class:`Channel` in the specified :class:`Server`.
@@ -2159,7 +2159,7 @@ class Client:
 
             perms.append(payload)
 
-        data = yield from self.http.create_channel(server.id, name, str(type), permission_overwrites=perms)
+        data = yield from self.http.create_channel(server.id, name, type.value, permission_overwrites=perms, parent=parent)
         channel = Channel(server=server, **data)
         return channel
 
